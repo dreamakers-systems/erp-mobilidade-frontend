cbe114d2712bc4f00fbbaaa6cd20c4c2f216dc5d
Mariano Tupa <marianotupa@gmail.com>
2022-03-31 23:09:44 -0300

    stable auth0

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,42 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  
 } from 'react-router-dom'
 
-import Home from './components/pages/Home'
-import Login from './components/pages/login/Login/'
 
+import { getConfig } from './configs';
+import Home from './components/pages/Home'
+import { Auth0Provider } from '@auth0/auth0-react';
+
+import { createBrowserHistory } from 'history';
+import { ProtectedRoute } from './components/protected-route';
+
+const config = getConfig();
+
+export const history = createBrowserHistory();
+
+const onRedirectCallback = (appState) => {
+  // Use the router's history module to replace the url
+  history.replace(appState?.returnTo || window.location.origin);
+};
+
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  redirectUri: window.location.origin,
+  
+};
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <Auth0Provider {...providerConfig} onRedirectCallback={onRedirectCallback}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/app" element={<ProtectedRoute component={Home}/>}/>
+        </Routes>
+      </Router>
+    </Auth0Provider>
   )
 }
 
